00b7858ee6e134e6e68e65a191ea96a97e908fba
friendsea <friendsea1995@gmail.com>
2019-03-03 02:07:43 +0900

    ISoundEffect

---
+++ Assets/Scripts/ISoundEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FriendSea
+{
+
+    public interface ISoundEffect
+    {
+        AudioClip Clip { get; }
+        float Volume { get; }
+        bool Spacial { get; }
+        bool Exclusive { get; }
+    }
+}
